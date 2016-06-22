ac3c567482dd5e673db8acff20887274648248e7
yossigi <yossigi@users.noreply.github.com>
2016-06-22 11:50:59 +0300

    discard opt + ready to play with timeout

--- pcc-gradient/sender/app/pcc.h
+++ pcc-gradient/sender/app/pcc.h
@@ -52,7 +52,7 @@ class Measurement {
 		MeasurementType type_;
 		int monitor_number_;
 };
-	
+
 class PCC : public CCC {
 public:
 	virtual ~PCC() {}
@@ -63,9 +63,9 @@ public:
 		}
 		return 0;
 	}
-	
+
 	virtual void onLoss(const int32_t*, const int&) {}
-	virtual bool onTimeout(int total, int loss, double in_time, int current, int endMonitor, double rtt){ 
+	virtual bool onTimeout(int total, int loss, double in_time, int current, int endMonitor, double rtt){
 		lock_guard<mutex> lck(monitor_mutex_);
 		//cout << "handling timeout in PCC! for monitor " << monitor << endl;
 		if (state_ != START) {
@@ -77,10 +77,10 @@ public:
 			}
 		} /*else if (monitor_in_start_phase_ != monitor) {
 			cout << "START: monitor " << monitor << " already gone! current monitor: " << monitor_in_start_phase_ << endl;
-			return false;			
+			return false;
 		}*/
-		
-	
+
+
 		kInTimeout = true;
 		long double curr_utility = utility(total, 0, in_time, rtt, NULL);
 		if (curr_utility > last_utility_) {
@@ -93,21 +93,16 @@ public:
 		cout << "current rate " << rate() << " --> ";
 		//#endif
 		decide(last_utility_, curr_utility, true);
-		
+
+
+
+		//setRate(0.75 * rate());
+		//base_rate_ = rate();
 		double r = rate();
 		//#ifdef DEBUG_PRINT
 			cout << "timeout! new rate is " << r << endl;
 		//#endif
-		restart(); 
-		state_ = SEARCH;
-		
-		if (r < 1.01 * kMinRateMbps) {
-			cout << "going to bed" << endl;
-			this_thread::sleep_for(std::chrono::seconds(5));
-			cout << "done sleeping" << endl;
-		}
-		
-		/*
+		restart();
 		if (r > 1.01 * kMinRateMbps) {
 			cout << "going to SEARCH rate = " << rate() << ". Thresh = " << 1.01 * kMinRateMbps << endl;
 			state_ = SEARCH;
@@ -118,12 +113,12 @@ public:
 			slow_start_factor_ = 2;
 			state_ = START;
 			setRate(base_rate_);
-			lock_guard<mutex> lck(data_lock_);
-			this_thread::sleep_for(std::chrono::seconds(1));
-			cout << "done sleeping;"
 		}
-		*/
+		//clear_state();
+		//start_measurment_map_.clear();
+		//end_measurment_map_.clear();
 		kInTimeout = false;
+		//cout << "new rate: " << rate() << base_rate_ << endl;
 		return false;
 	}
 	virtual void onACK(const int& ack){}
@@ -139,7 +134,7 @@ public:
 			start_measurement_ = true;
 		}
 	}
-	
+
 	virtual void onMonitorStart(int current_monitor) {
 		lock_guard<mutex> lck(monitor_mutex_);
 		//cout << "starting monitor " << current_monitor << endl;
@@ -179,7 +174,8 @@ public:
 			start_measurement_ = !start_measurement_;
 		}
 	}
-	
+
+
 	Measurement* get_monitor_measurement(int monitor) {
 		if (start_measurment_map_.find(monitor) != start_measurment_map_.end()) {
 			return start_measurment_map_.at(monitor).get();
@@ -190,7 +186,7 @@ public:
 		}
 		return NULL;
 	}
-	
+
 	virtual void onMonitorEnds(int total, int loss, double in_time, int current, int endMonitor, double rtt) {
 		lock_guard<mutex> lck(monitor_mutex_);
 		Measurement* this_measurement = get_monitor_measurement(endMonitor);
@@ -317,30 +313,30 @@ public:
 		kExponent = exponent;
 		kPolyUtility = polyUtility;
 	}
-	
+
 protected:
 
 	static double kAlpha, kBeta, kExponent;
 	static bool kPolyUtility;
-	
+
     long double search_monitor_utility[2];
     int search_monitor_number[2];
     bool start_measurement_;
 	double base_rate_;
 	bool kPrint;
 	static constexpr double kMinRateMbps = 0.5;
-	static constexpr double kMinRateMbpsSlowStart = 0.03;
+	static constexpr double kMinRateMbpsSlowStart = 0.05;
 	static constexpr double kMaxRateMbps = 1024.0;
 
 	enum ConnectionState {
 		START,
 		SEARCH
 	} state_;
-	
+
 
 	virtual void search() = 0;
 	virtual void decide(long double start_utility, long double end_utility, bool force_change) = 0;
-	
+
 	virtual void clear_state() {
 		continue_slow_start_ = true;
 		start_measurement_ = true;
@@ -365,7 +361,7 @@ protected:
 		state_ = START;
 		prev_utility_ = -10000000;
 	}
-	
+
 	PCC() : start_measurement_(true), base_rate_(kMinRateMbps), kPrint(false), state_(START), monitor_in_start_phase_(-1), slow_start_factor_(2),
 			alpha_(kAlpha), beta_(kBeta), exponent_(kExponent), poly_utlity_(kPolyUtility), rate_(kMinRateMbps), monitor_in_prog_(-1), utility_sum_(0), measurement_intervals_(0), prev_utility_(-10000000), continue_slow_start_(true), last_utility_(0), on_next_start_bind_to_end_(-1) {
 		m_dPktSndPeriod = 10000;
@@ -374,7 +370,7 @@ protected:
 		srand(time(NULL));
 		cout << "new Code!!!" << endl;
 		cout << "configuration: alpha = " << alpha_ << ", beta = " << beta_   << ", exponent = " << exponent_ << " poly utility = " << poly_utlity_ << endl;
-		
+
 		/*
 		if (!latency_mode) {
 			beta_ = 0;
@@ -388,27 +384,27 @@ protected:
 		if (base_rate_ > kMinRateMbps) {
 			return kMinRateMbps;
 		} else if (base_rate_ > kMinRateMbps / 2) {
-			return kMinRateMbps / 2; 
+			return kMinRateMbps / 2;
 		} else {
-			return 2 * kMinRateMbpsSlowStart; 
+			return 2 * kMinRateMbpsSlowStart;
 		}
 	}
 	virtual void setRate(double mbps) {
 		//cout << "set rate: " << rate_ << " --> " << mbps << endl;
 		if (state_ == START) {
-			if (mbps < kMinRateMbpsSlowStart){ 
+			if (mbps < kMinRateMbpsSlowStart){
 				#ifdef DEBUG_PRINT
 					cout << "rate is mimimal at slow start, changing to " << kMinRateMbpsSlowStart << " instead" << endl;
 				#endif
-				mbps = kMinRateMbpsSlowStart; 
-			} 
-		} else if (mbps < kMinRateMbps){ 
+				mbps = kMinRateMbpsSlowStart;
+			}
+		} else if (mbps < kMinRateMbps){
 			#ifdef DEBUG_PRINT
 				cout << "rate is mimimal, changing to " << kMinRateMbps << " instead" << endl;
 			#endif
-			mbps = kMinRateMbps; 
-		} 
-		
+			mbps = kMinRateMbps;
+		}
+
 		if (mbps > kMaxRateMbps) {
 			mbps = kMaxRateMbps;
 			cout << "rate is maximal, changing to " << kMaxRateMbps << " instead" << endl;
@@ -448,15 +444,15 @@ private:
 	}
 
 	bool sanety_check(Measurement* start, Measurement* end) {
-		
+
 		if (end->test_rate_ < start->test_rate_) {
-			//cout << "swapping. Rates: " << start->test_rate_ << ", " << end->test_rate_ << endl; 
+			//cout << "swapping. Rates: " << start->test_rate_ << ", " << end->test_rate_ << endl;
 			Measurement* swap_temp = start;
 			start = end;
 			end = swap_temp;
 		}
-		
-		if (start->loss_panelty_ < end->loss_panelty_) { 
+
+		if (start->loss_panelty_ < end->loss_panelty_) {
 			//cout << "failed on loss. Start = " << start->loss_panelty_ << ". End = " << end->loss_panelty_ << endl;
 			return false;
 		}
@@ -473,14 +469,14 @@ private:
 
 	virtual long double utility(unsigned long total, unsigned long loss, double time, double rtt, Measurement* out_measurement) {
 		static long double last_measurement_interval = 1;
-		
+
 		long double norm_measurement_interval = last_measurement_interval;
-		
+
 		if (!kInTimeout) {
 			norm_measurement_interval = time / rtt;
 			last_measurement_interval = norm_measurement_interval;
 		}
-		
+
 		rtt_history_.push_front(rtt);
 		if (rtt_history_.size() > kHistorySize) {
 			rtt_history_.pop_back();
@@ -490,11 +486,11 @@ private:
 		double rtt_penalty = rtt / get_min_rtt();
 		if (rtt_penalty > 4) rtt_penalty  = 4;
 		exponent_ = 2.5;
-	
+
 		long double loss_contribution = total * (long double) (alpha_* (pow((1+((long double)((double) loss/(double) total))), exponent_)-1));
-		long double rtt_contribution = 2 * total*(pow(rtt_penalty,1.6) - 1); 
+		long double rtt_contribution = 2 * total*(pow(rtt_penalty,1.6) - 1);
 		long double utility = ((long double)total - loss_contribution - rtt_contribution)/norm_measurement_interval;
-	
+
 		if (out_measurement != NULL) {
 			out_measurement->loss_panelty_ = loss_contribution / norm_measurement_interval;
 			out_measurement->rtt_panelty_ = rtt_contribution / norm_measurement_interval;
@@ -504,7 +500,7 @@ private:
 	}
 
 	static const long kMillisecondsDigit = 10 * 1000;
-	
+
 	int monitor_in_start_phase_;
 	double slow_start_factor_;
 	double alpha_;
