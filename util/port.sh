ee571d1906258232d87fde97b23d8104e665321d
Nathan Jay <njay2@illinois.edu>
2017-11-29 17:23:55 -0600

    Changed porting script to reflect new code structure.

--- util/port.sh
+++ util/port.sh
@@ -2,5 +2,5 @@
 
 sunifdef -DQUIC_PORT -DQUIC_PORT_LOCAL "$1/src/pcc/pcc_monitor_interval_queue.cpp" > "$2/pcc_monitor_interval_queue.cc"
 sunifdef -DQUIC_PORT -DQUIC_PORT_LOCAL "$1/src/pcc/pcc_monitor_interval_queue.h" > "$2/pcc_monitor_interval_queue.h"
-sunifdef -DQUIC_PORT -DQUIC_PORT_LOCAL "$1/src/pcc_sender.cpp" > "$2/pcc_sender.cc"
-sunifdef -DQUIC_PORT -DQUIC_PORT_LOCAL "$1/src/pcc_sender.h" > "$2/pcc_sender.h"
+sunifdef -DQUIC_PORT -DQUIC_PORT_LOCAL "$1/src/pcc/pcc_sender.cpp" > "$2/pcc_sender.cc"
+sunifdef -DQUIC_PORT -DQUIC_PORT_LOCAL "$1/src/pcc/pcc_sender.h" > "$2/pcc_sender.h"
