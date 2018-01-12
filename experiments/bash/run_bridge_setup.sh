aaf4b43df85dacf2a7d00f195d36fd5ea6e0889d
meng-tong <victor9966@163.com>
2018-01-12 15:16:39 -0600

    correct the changing pipe setup bug that crash the bridge

--- experiments/bash/run_bridge_setup.sh
+++ experiments/bash/run_bridge_setup.sh
@@ -16,7 +16,7 @@
 # $12: emulab username
 
 sudo ipfw delete 100
-sudo ipfw pipe 100 config bw ${4}Mbit/s delay ${6}ms queue ${8}KB
+sudo ipfw pipe 100 config bw ${4}Mbit/s delay ${6}ms queue ${8}KB plr ${10}
 
 echo "${1} pairs of sender/receiver"
 L=`jot ${1}`
@@ -43,23 +43,20 @@ RANDOM=1
 while [ 1 ]
 do
   # FIXME: pay attention to the maximum range of RANDOM (only up to 32767)
-  # bandwidth has the granularity of 100Kbps
-  l_bw=$(gawk -v x=${4} 'BEGIN{printf "%d\n", (x * 10)}')
-  r_bw=$(gawk -v x=${5} 'BEGIN{printf "%d\n", (x * 10)}')
-  bw_base=$((RANDOM % $((${r_bw}-${l_bw}+1)) + ${l_bw}))
-  bw=$(gawk -v x=${bw_base} 'BEGIN{printf "%.4f\n", (1.0*x/10.0)}')
+  # bandwidth has the granularity of 1Mbps
+  l_bw=$(gawk -v x=${4} 'BEGIN{printf "%d\n", (x)}')
+  r_bw=$(gawk -v x=${5} 'BEGIN{printf "%d\n", (x)}')
+  bw=$((RANDOM % $((${r_bw}-${l_bw}+1)) + ${l_bw}))
 
-  # delay has the granularity of 100us
-  l_dl=$(gawk -v x=${6} 'BEGIN{printf "%d\n", (x * 10)}')
-  r_dl=$(gawk -v x=${7} 'BEGIN{printf "%d\n", (x * 10)}')
-  dl_base=$((RANDOM % $((${r_dl}-${l_dl}+1)) + ${l_dl}))
-  dl=$(gawk -v x=${dl_base} 'BEGIN{printf "%.4f\n", (1.0*x/10.0)}')
+  # delay has the granularity of 1ms
+  l_dl=$(gawk -v x=${6} 'BEGIN{printf "%d\n", (x)}')
+  r_dl=$(gawk -v x=${7} 'BEGIN{printf "%d\n", (x)}')
+  dl=$((RANDOM % $((${r_dl}-${l_dl}+1)) + ${l_dl}))
 
-  # queue size has granularity of 10byte
-  l_qs=$(gawk -v x=${8} 'BEGIN{printf "%d\n", (x * 100)}')
-  r_qs=$(gawk -v x=${9} 'BEGIN{printf "%d\n", (x * 100)}')
-  qs_base=$((RANDOM % $((${r_qs}-${l_qs}+1)) + ${l_qs}))
-  qs=$(gawk -v x=${qs_base} 'BEGIN{printf "%.4f\n", (1.0*x/100.0)}')
+  # queue size has granularity of 1KB
+  l_qs=$(gawk -v x=${8} 'BEGIN{printf "%d\n", (x)}')
+  r_qs=$(gawk -v x=${9} 'BEGIN{printf "%d\n", (x)}')
+  qs=$((RANDOM % $((${r_qs}-${l_qs}+1)) + ${l_qs}))
 
   # loss rate has the granularity of 0.01%
   l_lr=$(gawk -v x=${10} 'BEGIN{printf "%d\n", (x * 10000)}')
@@ -68,8 +65,8 @@ do
   lr=$(gawk -v x=${lr_base} 'BEGIN{printf "%.4f\n", (1.0*x/10000.0)}')
 
   time_ms=$(date +%s)
-  #echo "${time_ms}: bw ${bw}Mbit/s delay ${dl}ms queue ${qs}KB plr ${lr}" >> \
-  #    /users/${12}/pcc_log_bridge_${start_time_ms}.txt
+  echo "${time_ms}: bw ${bw}Mbit/s delay ${dl}ms queue ${qs}KB plr ${lr}" >> \
+      /users/${12}/pcc_log_bridge_${start_time_ms}.txt
 
   sudo ipfw pipe 100 config bw ${bw}Mbit/s delay ${dl}ms queue ${qs}KB plr ${lr}
 
