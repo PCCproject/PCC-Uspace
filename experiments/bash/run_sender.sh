73af846e90bc0d01f090646ada427a2379ce63f9
Nathan Jay <njay2@illinois.edu>
2018-03-01 09:56:29 -0600

    Added printout for sleep time to sender script.

--- experiments/bash/run_sender.sh
+++ experiments/bash/run_sender.sh
@@ -20,5 +20,6 @@ echo "Timestamp: ${time_ms}"
 echo " $1 send $2 9000 -DEBUG_RATE_CONTROL -DEBUG_UTILITY_CALC -LOG_RATE_CONTROL_PARAMS -log=$3/pcc_log_${time_ms}.txt -npairs=$4 -bandwidth=$5 -delay=$6 -queue=$7 -loss=$8 ${@:12} 1>$9 2>${10} & PID=$!  "
 $1 send $2 9000 -DEBUG_RATE_CONTROL -DEBUG_UTILITY_CALC -LOG_RATE_CONTROL_PARAMS -log=$3/pcc_log_${time_ms}.txt -npairs=$4 -bandwidth=$5 -delay=$6 -queue=$7 -loss=$8 ${@:12} 1>$9 2>${10} & PID=$!
 
+echo "Sleeping for ${11}s"
 sleep ${11}
 sudo kill -SIGINT $PID
