66c15896fa3923fb1c4ba524f6883a10a85ddd02
Nathan Jay <njay2@ocean0.cs.illinois.edu>
2019-01-19 11:09:11 -0600

    Added batch training scripts and various command line arguments

--- python/env/mlp/stable_solve.py
+++ python/env/mlp/stable_solve.py
@@ -8,10 +8,17 @@ from stable_baselines.common.policies import FeedForwardPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines import PPO1
 from stable_baselines import TRPO
+from simple_arg_parse import arg_or_default
 
+arch_str = arg_or_default("--arch", default="32,16")
+if arch_str == "":
+    arch = []
+else:
+    arch = [int(layer_width) for layer_width in arch_str.split(",")]
+print("Architecture is: %s" % str(arch))
 class MyMlpPolicy(FeedForwardPolicy):
 
-    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, net_arch=[32, 16], reuse=False, **_kwargs):
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch, net_arch=arch, reuse=False, **_kwargs):
         super(MyMlpPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse,
                                         feature_extraction="mlp", **_kwargs)
 
@@ -19,13 +26,16 @@ n_cpu = 1
 env = gym.make('PccNs-v0')
 #env = gym.make('CartPole-v0')
 
-model = PPO1(MyMlpPolicy, env, verbose=1, schedule='constant', timesteps_per_actorbatch=8192, optim_batchsize=2048)
-model.learn(total_timesteps=(4800 * 410))
+gamma = arg_or_default("--gamma", default=0.99)
+print("gamma = %f" % gamma)
+model = PPO1(MyMlpPolicy, env, verbose=1, schedule='constant', timesteps_per_actorbatch=8192, optim_batchsize=2048, gamma=gamma)
+model.learn(total_timesteps=(9600 * 410))
 
 ##
 #   Save the model to the location specified below.
 ##
-export_dir = "/home/pcc/PCC/deep-learning/python/saved_models/mlp_sf_10hist_wide_queue/"
+default_export_dir = "/tmp/pcc_saved_models/model_A/"
+export_dir = arg_or_default("--model-dir", default=default_export_dir)
 with model.graph.as_default():
 
     pol = model.policy_pi#act_model
